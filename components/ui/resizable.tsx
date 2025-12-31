60bf355fc4882ef99e06417dd807b25626c96216
janpaul80 <hello@jphartman.com>
2025-12-31 08:05:55 -0800

    Fix: Downgrade react-resizable-panels to 2.1.7 and revert UI components to standard implementation

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -1,16 +1,17 @@
 "use client"
-// Shadcn UI Resizable with forwardRef and explicit types to fix deployment build errors
 
 import * as React from "react"
 import { GripVerticalIcon } from "lucide-react"
-import { Group as PanelGroup, Panel, Separator as PanelResizeHandle } from "react-resizable-panels"
+import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
-const ResizablePanelGroup = React.forwardRef<any, any>(
-  ({ className, ...props }, ref) => (
+function ResizablePanelGroup({
+  className,
+  ...props
+}: React.ComponentProps<typeof PanelGroup>) {
+  return (
     <PanelGroup
-      ref={ref}
       data-slot="resizable-panel-group"
       className={cn(
         "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
@@ -19,18 +20,23 @@ const ResizablePanelGroup = React.forwardRef<any, any>(
       {...props}
     />
   )
-)
+}
 
-const ResizablePanel = React.forwardRef<any, any>(
-  ({ ...props }, ref) => (
-    <Panel ref={ref} data-slot="resizable-panel" {...props} />
-  )
-)
+function ResizablePanel({
+  ...props
+}: React.ComponentProps<typeof Panel>) {
+  return <Panel data-slot="resizable-panel" {...props} />
+}
 
-const ResizableHandle = React.forwardRef<any, any>(
-  ({ withHandle, className, ...props }, ref) => (
+function ResizableHandle({
+  withHandle,
+  className,
+  ...props
+}: React.ComponentProps<typeof PanelResizeHandle> & {
+  withHandle?: boolean
+}) {
+  return (
     <PanelResizeHandle
-      ref={ref}
       data-slot="resizable-handle"
       className={cn(
         "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90",
@@ -45,6 +51,6 @@ const ResizableHandle = React.forwardRef<any, any>(
       )}
     </PanelResizeHandle>
   )
-)
+}
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
