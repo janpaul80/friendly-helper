8548f438b5e9deab8609362c5e6736b3f4a8d4cc
janpaul80 <hello@jphartman.com>
2025-12-31 07:10:32 -0800

    Fix: Unified ResizablePanelGroup typing with forwardRef and explicit component props

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -6,53 +6,49 @@ import { Group as PanelGroup, Panel, Separator as PanelResizeHandle } from "reac
 
 import { cn } from "@/lib/utils"
 
-function ResizablePanelGroup({
-  className,
-  ...props
-}: React.ComponentProps<typeof PanelGroup>) {
-  return (
-    <PanelGroup
-      data-slot="resizable-panel-group"
-      className={cn(
-        "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
-        className
-      )}
-      {...props}
-    />
-  )
-}
+const ResizablePanelGroup = React.forwardRef<
+  React.ElementRef<typeof PanelGroup>,
+  React.ComponentPropsWithoutRef<typeof PanelGroup>
+>(({ className, ...props }, ref) => (
+  <PanelGroup
+    ref={ref}
+    data-slot="resizable-panel-group"
+    className={cn(
+      "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
+      className
+    )}
+    {...props}
+  />
+))
 
-import type { PanelProps, PanelResizeHandleProps } from "react-resizable-panels"
+const ResizablePanel = React.forwardRef<
+  React.ElementRef<typeof Panel>,
+  React.ComponentPropsWithoutRef<typeof Panel>
+>(({ ...props }, ref) => (
+  <Panel ref={ref} data-slot="resizable-panel" {...props} />
+))
 
-function ResizablePanel({
-  ...props
-}: React.ComponentProps<typeof Panel>) {
-  return <Panel data-slot="resizable-panel" {...props} />
-}
-
-function ResizableHandle({
-  withHandle,
-  className,
-  ...props
-}: React.ComponentProps<typeof PanelResizeHandle> & {
-  withHandle?: boolean
-}) {
-  return (
-    <PanelResizeHandle
-      data-slot="resizable-handle"
-      className={cn(
-        "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90",
-        className
-      )}
-      {...props}
-    >
-      {withHandle && (
-        <div className="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-xs border">
-          <GripVerticalIcon className="size-2.5" />
-        </div>
-      )}
-    </PanelResizeHandle>
-  )
-}
+const ResizableHandle = React.forwardRef<
+  React.ElementRef<typeof PanelResizeHandle>,
+  React.ComponentPropsWithoutRef<typeof PanelResizeHandle> & {
+    withHandle?: boolean
+  }
+>(({ withHandle, className, ...props }, ref) => (
+  <PanelResizeHandle
+    ref={ref}
+    data-slot="resizable-handle"
+    className={cn(
+      "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90",
+      className
+    )}
+    {...props}
+  >
+    {withHandle && (
+      <div className="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-xs border">
+        <GripVerticalIcon className="size-2.5" />
+      </div>
+    )}
+  </PanelResizeHandle>
+))
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
